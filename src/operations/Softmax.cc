a8bc71a8037c383017bd4575f7ddd6cfcd9db034
Wonhyuk Yang <wonhyuk@postech.ac.kr>
2024-03-27 07:01:34 +0000

    Add softmax operation
    Note, this operation is used for attention.cc

---
+++ src/operations/Softmax.cc
@@ -0,0 +1,105 @@
+#include "Softmax.h"
+#include "../Model.h"
+
+Softmax::Softmax(SimulationConfig config, Model* model,
+               onnx::NodeProto& node_proto)
+    : Operation(config, model, node_proto) {
+
+    /* Load weight info from node */
+    _input_shape = get_input(0)->get_dims();
+
+    assert(_input_shape.size()==2);
+    _seq = _input_shape.at(0);
+    _dk = _input_shape.at(1);
+
+    for (int i=0;i<node_proto.output().size();i++) {
+        _output_shape = _input_shape;
+        if (node_proto.output(i)=="")
+            continue;
+
+        Tensor* pre_defind_tensor = _model->find_tensor(node_proto.output(i));
+        if (pre_defind_tensor == nullptr) {
+            std::unique_ptr<Tensor> output_tensor = std::make_unique<Tensor>(
+                _id, node_proto.output(i), _output_shape, false);
+                _outputs.push_back(output_tensor.get()->get_id());
+            _model->add_tensor(std::move(output_tensor));
+        } else {
+            pre_defind_tensor->redefine_tensor(_id, _output_shape);
+        }
+    }
+    calculate_loops();
+}
+
+Softmax::Softmax(SimulationConfig config, MappingTable mapping_table,
+           std::vector<uint32_t> input_shape)
+    : Operation(config, mapping_table) {
+    _input_shape = input_shape;
+    _output_shape = input_shape;
+
+    assert(_input_shape.size()==2);
+    _seq = _input_shape.at(0);
+    _dk = _input_shape.at(1);
+    calculate_loops();
+}
+
+void Softmax::initialize_tiles(MappingTable mapping_table) {
+    for (uint32_t tokens=0; tokens < _seq; tokens+=_tokens_per_tile) {
+        uint32_t remain_tokens = std::min(_seq-tokens, _tokens_per_tile);
+        auto tile = Tile{
+            .status = Tile::Status::INITIALIZED,
+            .optype = get_name(),
+            .layer_id = _id,
+            .accum = false,
+        };
+        /* dummy mapping */
+        Mapping mapping;
+        initialize_instructions(tile, mapping, tokens, remain_tokens);
+
+        _tiles.push_back(tile);
+    }
+}
+
+void Softmax::initialize_instructions(Tile &tile, Mapping mapping, uint32_t token_offset, uint32_t tokens) {
+    addr_type sram_base = SPAD_BASE;
+
+    /* Load two tile (input: tokens x _dk, skip: tokens x _dk) */
+    std::set<addr_type> dram_addrs;
+    int offset;
+    for (offset=0; offset<tokens*_dk*_config.precision; offset+=_config.dram_req_size)
+        dram_addrs.insert(token_offset*_dk*_config.precision + offset);
+
+    tile.instructions.push_back(Instruction{
+        .opcode = Opcode::MOVIN,
+        .dest_addr = sram_base,
+        .size = (uint32_t)dram_addrs.size(),
+        .src_addrs = std::vector<addr_type>(dram_addrs.begin(), dram_addrs.end()),
+        .operand_id = _INPUT_OPERAND,  // query
+    });
+
+    tile.instructions.push_back(Instruction{
+        .opcode = Opcode::SOFTMAX,
+        .dest_addr = sram_base,
+        .size = _dk * _config.precision,
+        .src_addrs = std::vector<addr_type>{sram_base},
+        .tile_m = tokens,
+    });
+
+    tile.instructions.push_back(Instruction{
+        .opcode = Opcode::MOVOUT,
+        .dest_addr = sram_base,
+        .size = (uint32_t)dram_addrs.size(),
+        .src_addrs = std::vector<addr_type>(dram_addrs.begin(), dram_addrs.end()),
+        .operand_id = _OUTPUT_OPERAND,
+    });
+}
+
+void Softmax::calculate_loops() {
+    uint32_t size_per_token = _dk * 2 * _config.precision;
+    uint32_t sram_capacity = _config.spad_size KB / 2;  // unit: byte
+
+    _tokens_per_tile = sram_capacity / size_per_token;
+    assert (_tokens_per_tile >= 1);
+    if (_tokens_per_tile > _seq) _tokens_per_tile = _seq;
+
+    spdlog::info("[Softmax] tokens_per_tile: {}", _tokens_per_tile);
+}
---
+++ src/operations/Softmax.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Operation.h"
+
+class Softmax : public Operation {
+public:
+    Softmax(SimulationConfig config, Model* model, onnx::NodeProto& node_proto);
+    Softmax(SimulationConfig config, MappingTable mapping_table,
+       std::vector<uint32_t> input_shape);
+    std::vector<uint32_t> _input_shape;
+    std::vector<uint32_t> _output_shape;
+
+    uint32_t _seq;
+    uint32_t _dk;
+    uint32_t _tokens_per_tile;
+
+    void calculate_loops();
+    void initialize_tiles(MappingTable mapping_table) override;
+    void initialize_instructions(Tile& tile, Mapping mapping, uint32_t token_offset, uint32_t tokens);
+};
