feec09ab71909d81b32a24afaf0f29be498eb190
Wonhyuk Yang <wonhyuk@postech.ac.kr>
2024-03-28 13:18:53 +0000

    Add script for slurm batch

---
+++ scripts/onnxim_sbatch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -p allcpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH -t 03:00:00
+
+ml swap gnu8 gnu13
+which gcc
+
+echo "config: $1 model: $2"
+export ONNXIM_HOME="/home/wonhyuks/ONNXim-CAL/ONNXim"
+echo "$ONNXIM_HOME/build/bin/Simulator --config $ONNXIM_HOME/configs/$1.json --model $ONNXIM_HOME/model_lists/$2.json"
+$ONNXIM_HOME/build/bin/Simulator --config $ONNXIM_HOME/configs/$1.json --model $ONNXIM_HOME/model_lists/$2.json
+
+exit 0
---
+++ scripts/run_sbatch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+models=("gpt2_s" "gpt2_g" "bert")
+batch_list=("1" "2" "4" "8" "16" "32")
+i=3
+configs=("systolic_ws_128x128_c4_simple_noc_tpuv4") # "systolic_ws_128x128_c4_booksim2_tpuv4")
+
+if [ ! -d "$ONNXIM_HOME/results" ]; then
+    mkdir $ONNXIM_HOME/results
+fi
+
+for model_file in "${models[@]}"; do
+    if [ ! -d "$ONNXIM_HOME/results/$model_file" ]; then
+        mkdir $ONNXIM_HOME/results/$model_file
+    fi
+    if [[ $model_file == "gpt2_g" ]] || [[ $model_file == "gpt2_s" ]]; then
+        onnx_file="gpt2"
+    elif [[ $model_file == "bert" ]]; then
+        onnx_file="$model_file"
+    fi
+    for batch in "${batch_list[@]}"; do
+        if [ ! -d "$ONNXIM_HOME/results/$model_file/$batch" ]; then
+            mkdir $ONNXIM_HOME/results/$model_file/$batch
+        fi
+        for config in "${configs[@]}"; do
+            if [ ! -d "$ONNXIM_HOME/results/$model_file/$batch/$config" ]; then
+                mkdir $ONNXIM_HOME/results/$model_file/$batch/$config
+            fi
+            for (( j=0; j<i; j++ )); do
+                echo "sbatch -J ONNXIM-$model_file -o $ONNXIM_HOME/results/$model_file/$batch/$config/result_$j.out  -e $ONNXIM_HOME/results/$model_file/$batch/$config/result_$j.err $ONNXIM_HOME/scripts/onnxim_sbatch.sh $config "$model_file"_$batch"
+                sbatch -J ONNXIM-$model_file -o $ONNXIM_HOME/results/$model_file/$batch/$config/result_$j.out  -e $ONNXIM_HOME/results/$model_file/$batch/$config/result_$j.err $ONNXIM_HOME/scripts/onnxim_sbatch.sh $config "$model_file"_$batch
+            done
+        done
+    done
+done
