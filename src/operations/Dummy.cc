830cc22cb9d7dd5694ceebfa697b8441a5c116a9
Wonhyuk Yang <wonhyuk@postech.ac.kr>
2024-03-29 06:35:19 +0000

    Add multi output feature for dummy operation

--- src/operations/Dummy.cc
+++ src/operations/Dummy.cc
@@ -9,32 +9,25 @@ Dummy::Dummy(SimulationConfig config, Model* model, onnx::NodeProto& node_proto)
   spdlog::trace("output_shape : {}", _output_shape);
   spdlog::trace("output name : {} {}", node_proto.output(0).c_str());
 
-  Tensor* pre_defind_tensor = _model->find_tensor(node_proto.output(0));
-  if (pre_defind_tensor == nullptr) {
-    std::unique_ptr<Tensor> output_tensor = std::make_unique<Tensor>(
-        _id, node_proto.output(0), _output_shape, false);
-    _outputs.push_back(output_tensor.get()->get_id());
-    _model->add_tensor(std::move(output_tensor));
-  } else {
-    pre_defind_tensor->redefine_tensor(_id, _output_shape);
+  for (int i=0;i<node_proto.output().size();i++) {
+    Tensor* pre_defind_tensor = _model->find_tensor(node_proto.output(i));
+    if (pre_defind_tensor == nullptr) {
+      std::unique_ptr<Tensor> output_tensor = std::make_unique<Tensor>(
+          _id, node_proto.output(i), _output_shape, false);
+      _outputs.push_back(output_tensor.get()->get_id());
+      _model->add_tensor(std::move(output_tensor));
+    } else {
+      pre_defind_tensor->redefine_tensor(_id, _output_shape);
+    }
   }
-  /* Calculate tile size */
-  _total_loop = std::accumulate(_input_shape.begin(), _input_shape.end(), 1U, [](uint32_t a, uint32_t b) {
-    return (uint64_t)a * (uint64_t)b;
-  });
-  _element_in_tile = _config.spad_size / (config.precision * 2); // Doubled buffer
 }
 
 void Dummy::initialize_tiles(MappingTable mapping_table) {
-  for (uint64_t i=0; i<_total_loop; i+=_element_in_tile) {
-    uint32_t remainder = std::min(_element_in_tile, (uint32_t(_total_loop - i)));
-    _tiles.push_back(Tile{.status = Tile::Status::INITIALIZED,
-                          .optype="Dummy",
-                          .layer_id=_id,
-                          .C = remainder,
-                          .skip = true});
-    initialize_instructions(_tiles.back(), Mapping{});
-  }
+  _tiles.push_back(Tile{.status = Tile::Status::INITIALIZED,
+                        .optype="Dummy",
+                        .layer_id=_id,
+                        .skip = true});
+  initialize_instructions(_tiles.back(), Mapping{});
 }
 
 void Dummy::initialize_instructions(Tile& tile, Mapping mapping) {
