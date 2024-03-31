aa733324acb20be5245427db7aa4f601c0121f6f
Wonhyuk Yang <wonhyuk@postech.ac.kr>
2024-03-31 13:26:23 +0000

    update multi tenancy script

--- scripts/generate_multi-tenancy_onnx2.py
+++ scripts/generate_multi-tenancy_onnx2.py
@@ -10,6 +10,11 @@ parser.add_argument('--gpts_ms', default=0)
 parser.add_argument('--gptg_ms', default=0)
 parser.add_argument('--bert_ms', default=0)
 parser.add_argument('--total_ms')
+
+parser.add_argument('--resnet_p', default=0)
+parser.add_argument('--gpts_p', default=0)
+parser.add_argument('--gptg_p', default=0)
+parser.add_argument('--bert_p', default=0)
 args = parser.parse_args()
 
 resnet_ms = int(args.resnet_ms)
@@ -17,6 +22,10 @@ gptg_ms = int(args.gptg_ms)
 gpts_ms = int(args.gpts_ms)
 bert_ms = int(args.bert_ms)
 total_ms = int(args.total_ms)
+resnet_partition = int(args.resnet_p)
+gptg_partition = int(args.gptg_p)
+gpts_partition = int(args.gpts_p)
+bert_partition = int(args.bert_p)
 
 pathlib.Path(f"{HOME}/model_lists").mkdir(parents=True, exist_ok=True)
 config = {
@@ -29,7 +38,8 @@ if resnet_ms:
     model_config = {
       "name": "resnet50",
       "batch_size": 1,
-      "request_time": i
+      "request_time": i,
+      "partition_id": resnet_partition
     }
     config["models"].append(model_config)
 
@@ -43,7 +53,8 @@ if gptg_ms:
       "seq_len": 1,
       "past_seq_len": 1023,
       "total_seq_len": 1024,
-      "request_time": i
+      "request_time": i,
+      "partition_id": gptg_partition
     }
     config["models"].append(model_config)
 
@@ -57,7 +68,8 @@ if gpts_ms:
       "seq_len": 1024,
       "past_seq_len": 0,
       "total_seq_len": 1024,
-      "request_time": i
+      "request_time": i,
+      "partition_id": gpts_partition
     }
     config["models"].append(model_config)
 
@@ -71,10 +83,11 @@ if bert_ms:
       "seq_len": 1024,
       "past_seq_len": 0,
       "total_seq_len": 1024,
-      "request_time": i
+      "request_time": i,
+      "partition_id": bert_partition
     }
     config["models"].append(model_config)
 
-with open(f"{HOME}/model_lists/multi_{resnet_ms}_{gpts_ms}_{gptg_ms}_{bert_ms}_{total_ms}.json", "w") as json_file:
+with open(f"{HOME}/model_lists/multi_{resnet_ms}_{gpts_ms}_{gptg_ms}_{bert_ms}_{total_ms}_{resnet_partition}{gpts_partition}{gptg_partition}{bert_partition}.json", "w") as json_file:
   json.dump(config, json_file, indent=4)
 print("DONE")
